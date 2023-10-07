ad1583890a90bc973b9d90f5e0fa0c39482667e8
Paco Au-Yeung <paco20071001@gmail.com>
2023-10-07 21:56:09 +0800

    Implement Sock5 agent for Proxy Passthrough

--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
 import express, { Express, Request, Response } from 'express';
 import {createProxyMiddleware} from "http-proxy-middleware";
 import dotenv from 'dotenv';
+import SocksProxyAgent from 'socks-proxy-agent';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const WARP_PROXY = process.env.WARP_PROXY;
 const CHATGPT_URL = "https://chat.openai.com/"
+// Create a SOCKS5 proxy agent
+const SockAgent = new SocksProxyAgent.SocksProxyAgent(WARP_PROXY!);
 app.get('/', (req: Request, res: Response) => {
     res.send('GPT Proxy');
 });
@@ -14,6 +18,7 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/chat', createProxyMiddleware({
   target: CHATGPT_URL,
   changeOrigin: true,
+    agent: SockAgent,
     pathRewrite: {
         [`^/chat`]: '',
     }
